03e4927b780faa9cb7f893e8372c74ccd465ab54
SPeshov <stojan.peshov@gmail.com>
2022-06-16 15:44:21 +0200

    add front end logic to submit mock data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,59 +1,91 @@
-import Head from 'next/head';
 import Image from 'next/image';
+// import MultiStep from 'react-multistep';
 
 import styles from '@/styles/Home.module.css';
+import { useState } from 'react';
+import { Person } from './api/person';
+
+// import { StepOne } from '@/components/stepOne';
+// import { StepTwo } from '@/components/stepTwo';
+
+// Input:
+// We ask them about their interests, availability, English confidence level, location,
+// Matching on:
+// matching  similar interests, availability, etc.
+// We also record data about what was the topic of the events that they took part in, who else was in those events, etc.
+
+// const steps = [
+//   { name: `Name`, component: <StepOne /> },
+//   { name: `Name`, component: <StepTwo /> },
+// ];
 
 export default function Home() {
+  const [data, setData] = useState<[string, number][]>([]);
+
+  const mockUserData: Person = {
+    name: `Robert Galusha`,
+    pos: [29.713645, -95.534338],
+    tags: [`vast`, `depressed`],
+    level: 2,
+    availability: 1,
+  };
+  const [userData, setUserData] = useState<Person>();
+
+  const postData = async (person: Person) => {
+    try {
+      const response = await fetch(`/api/person`, {
+        method: `POST`,
+        headers: {
+          'Content-Type': `application/json`,
+        },
+        body: JSON.stringify({ person }),
+      });
+
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status}`);
+      }
+
+      const people = await response.json();
+      setData(people.usersToFollow);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleSubmitData = () => {
+    setUserData(mockUserData);
+    postData(mockUserData);
+  };
+
   return (
     <div className={styles.container}>
-      <Head>
-        <title>TypeScript starter for Next.js</title>
-        <meta
-          name="description"
-          content="TypeScript starter for Next.js that includes all you need to build amazing apps"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{` `}
-          <code className={styles.code}>src/pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=typescript-nextjs-starter"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <p>User input:</p>
+        <p>{JSON.stringify(userData)}</p>
+        <button onClick={() => handleSubmitData()}>Submit user data</button>
+        <p>Users to follow:</p>
+        {data.length > 0 ? (
+          <div style={{ textAlign: `center` }}>
+            <div
+              style={{
+                marginTop: `1rem`,
+                display: `flex`,
+                justifyContent: `center`,
+              }}
+            >
+              {data.map((person, index) => (
+                <span
+                  key={index}
+                  style={{
+                    padding: `5px 10px`,
+                    border: `1px solid black`,
+                  }}
+                >{`${person[0]} / ${person[1]}`}</span>
+              ))}
+            </div>
+          </div>
+        ) : null}
+        {/* <MultiStep steps={steps} /> */}
       </main>
 
       <footer className={styles.footer}>
